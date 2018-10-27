7467bbcece7a64a674f25974df3e2211f9bb4882
Satish <satishmwaghela@gmail.com>
2018-10-27 16:55:39 +0530

    Changed cron form field's layout and css

--- webservice/src/main/resources/app/scripts/libs/cron/index.js
+++ webservice/src/main/resources/app/scripts/libs/cron/index.js
@@ -42,12 +42,12 @@ function getPeriodOptions() {
       label: 'month',
       value: 'month',
       prep: 'on the'
-    },
+    }/*,
     {
       label: 'year',
       value: 'year',
       prep: 'on the'
-    }
+    }*/
   ];
 }
 
@@ -58,7 +58,7 @@ const toDisplay = {
   "day"    : ["time"],
   "week"   : ["dow", "time"],
   "month"  : ["dom", "time"],
-  "year"   : ["dom", "month", "time"]
+  "year"   : ["dom", "mon", "time"]
 };
 
 const combinations = {
@@ -169,15 +169,15 @@ function getDayCron(value) {
 }
 
 function getWeekCron(value) {
-  return `${value.min} ${value.hour} * * ${value.day}`;
+  return `${value.min} ${value.hour} * * ${value.dow}`;
 }
 
 function getMonthCron(value) {
-  return `${value.min} ${value.hour} ${value.day} * *`;
+  return `${value.min} ${value.hour} ${value.dom} * *`;
 }
 
 function getYearCron(value) {
-  return `${value.min} ${value.hour} ${value.day} ${value.mon} *`;
+  return `${value.min} ${value.hour} ${value.dom} ${value.mon} *`;
 }
 
 function getCron(state) {
@@ -251,7 +251,7 @@ export default class ReactCron extends Component {
 
   onDayOptionSelect = (key) => {
     return (event) => {
-      const value = event.target.value;
+      const value = event.target.value || event.target.value;
       const obj = {};
       obj[key] = value;
       const { selectedDayOption } = this.state;
@@ -264,7 +264,7 @@ export default class ReactCron extends Component {
 
   onWeekOptionSelect = (key) => {
     return (event) => {
-      const value = event.target.value;
+      const value = event.target.value || event.target.dataset.value;
       const obj = {};
       obj[key] = value;
       const { selectedWeekOption } = this.state;
@@ -277,7 +277,7 @@ export default class ReactCron extends Component {
 
   onMonthOptionSelect = (key) => {
     return (event) => {
-      const value = event.target.value;
+      const value = event.target.value || event.target.dataset.value;
       const obj = {};
       obj[key] = value;
       const { selectedMonthOption } = this.state;
@@ -290,7 +290,7 @@ export default class ReactCron extends Component {
 
   onYearOptionSelect = (key) => {
     return (event) => {
-      const value = event.target.value;
+      const value = event.target.value || event.target.dataset.value;
       const obj = {};
       obj[key] = value;
       const { selectedYearOption } = this.state;
@@ -309,105 +309,219 @@ export default class ReactCron extends Component {
     };
   }
 
-  getHourComponent = () => {
+  getTextComp(str){
+    return <div className="cron-string">{str}</div>;
+  }
+
+  findHourOption(hour){
+    const {hourOptions} = this.state;
+    return hourOptions.find((h) => {
+      return h.value == hour;
+    });
+  }
+
+  findMinuteOption(min){
+    const {minuteOptions} = this.state;
+    return minuteOptions.find((h) => {
+      return h.value == min;
+    });
+  }
+
+  getHourSelect(selectedOption, onChangeCB){
+    const {disabled} = this.props;
+    const { hourOptions } = this.state;
+
+    return (
+      <select value={selectedOption.hour} onChange={onChangeCB} disabled={disabled}>
+        {hourOptions.map(this.getOptionComponent('hour_option'))}
+      </select>
+    );
+  }
+  getMinuteSelect(selectedOption, onChangeCB){
+    const {disabled} = this.props;
+    const { minuteOptions } = this.state;
+
+    return (
+      <select value={selectedOption.min} onChange={onChangeCB} disabled={disabled}>
+        {minuteOptions.map(this.getOptionComponent('minute_option'))}
+      </select>
+    );
+  }
+  getBadgeOptions(options, value, substrVal, onClick){
+    const {disabled} = this.props;
+    const optionComps = [];
+    options.forEach((o) => {
+      let strVal = o.label;
+      if(substrVal){
+        strVal = strVal.substr(0, substrVal);
+      }
+      const comp = <span
+        className={`cron-badge-option ${o.value == value ? 'active' : ''} ${disabled ? 'disabled' : ''}`}
+        data-value={o.value}
+        onClick={disabled ? null : onClick}
+      >
+        {strVal}
+      </span>;
+      optionComps.push(comp);
+    });
+    return optionComps;
+  }
+
+  getMinuteComponent = (cronPeriodString) => {
+    const {disabled} = this.props;
+    const { minuteOptions, selectedHourOption } = this.state;
+
+    return (
+      (this.state.selectedPeriod === 'minute') &&
+      <cron-minute-component>
+        {this.getTextComp(`${cronPeriodString}`)}
+      </cron-minute-component>
+    );
+  }
+
+  getHourComponent = (cronPeriodString) => {
     const {disabled} = this.props;
     const { minuteOptions, selectedHourOption } = this.state;
 
     return (
       (this.state.selectedPeriod === 'hour') &&
       <cron-hour-component>
-        <select value={selectedHourOption.min} onChange={this.onHourOptionSelect('min')} disabled={disabled} className='m-r-xs'>
-          {minuteOptions.map(this.getOptionComponent('minute_option'))}
-        </select>
-        minutes past the hour
+        <span className='m-l-xs'>Minute : </span>
+        {this.getMinuteSelect(selectedHourOption, this.onHourOptionSelect('min'))}
+        {this.getTextComp(`${cronPeriodString} ${selectedHourOption.min} minute past the hour`)}
       </cron-hour-component>
     );
   }
 
-  getDayComponent = () => {
+  getDayComponent = (cronPeriodString) => {
     const {disabled} = this.props;
     const { hourOptions, minuteOptions, selectedDayOption } = this.state;
+
+    const hourLabel = this.findHourOption(selectedDayOption.hour).label;
+    const minuteLabel = this.findMinuteOption(selectedDayOption.min).label;
 
     return (
       (this.state.selectedPeriod === 'day') &&
       <cron-day-component>
-        <select value={selectedDayOption.hour} onChange={this.onDayOptionSelect('hour')} disabled={disabled}>
-          {hourOptions.map(this.getOptionComponent('hour_option'))}
-        </select>
+        <span className='m-l-xs'>Time : </span>
+        {this.getHourSelect(selectedDayOption, this.onDayOptionSelect('hour'))}
         :
-        <select value={selectedDayOption.min} onChange={this.onDayOptionSelect('min')}>
-          {minuteOptions.map(this.getOptionComponent('minute_option'))}
-        </select>
+        {this.getMinuteSelect(selectedDayOption, this.onDayOptionSelect('min'))}
+        {this.getTextComp(`${cronPeriodString} at ${hourLabel}:${minuteLabel}`)}
       </cron-day-component>
     );
   }
 
-  getWeekComponent = () => {
+  getWeekComponent = (cronPeriodString) => {
     const {disabled} = this.props;
     const { hourOptions, minuteOptions, dayOptions, selectedWeekOption } = this.state;
+
+    const hourLabel = this.findHourOption(selectedWeekOption.hour).label;
+    const minuteLabel = this.findMinuteOption(selectedWeekOption.min).label;
+
+    const dayLabel = dayOptions.find((d) => {
+      return d.value == selectedWeekOption.dow;
+    }).label;
 
     return (
       (this.state.selectedPeriod === 'week') &&
       <cron-week-component>
-        <select value={selectedWeekOption.day} onChange={this.onWeekOptionSelect('day')} disabled={disabled}>
-          {dayOptions.map(this.getOptionComponent('week_option'))}
-        </select>
-        <span className='m-l-xs m-r-xs'>at</span>
-        <select value={selectedWeekOption.hour} onChange={this.onWeekOptionSelect('hour')} disabled={disabled}>
-          {hourOptions.map(this.getOptionComponent('hour_option'))}
-        </select>
-        :
-        <select value={selectedWeekOption.min} onChange={this.onWeekOptionSelect('min')} disabled={disabled}>
-          {minuteOptions.map(this.getOptionComponent('minute_option'))}
-        </select>
+        <div className="cron-field-row">
+          <span className='m-l-xs'>Day : </span>
+          {/*<select value={selectedWeekOption.dow} onChange={this.onWeekOptionSelect('dow')} disabled={disabled}>
+            {dayOptions.map(this.getOptionComponent('week_option'))}
+          </select>*/}
+          <div className="cron-badge-option-container week-opt-container">
+            {this.getBadgeOptions(dayOptions, selectedWeekOption.dow, 1, this.onWeekOptionSelect('dow'))}
+          </div>
+        </div>
+        <div className="cron-field-row">
+          <span className='m-l-xs'>Time : </span>
+          {this.getHourSelect(selectedWeekOption, this.onWeekOptionSelect('hour'))}
+          :
+          {this.getMinuteSelect(selectedWeekOption, this.onWeekOptionSelect('min'))}
+        </div>
+        {this.getTextComp(`${cronPeriodString} on ${dayLabel} at ${hourLabel}:${minuteLabel}`)}
       </cron-week-component>
     );
   }
 
-  getMonthComponent = () => {
+  getMonthComponent = (cronPeriodString) => {
     const {disabled} = this.props;
     const { monthDaysOptions, hourOptions, minuteOptions, selectedMonthOption } = this.state;
+
+    const hourLabel = this.findHourOption(selectedMonthOption.hour).label;
+    const minuteLabel = this.findMinuteOption(selectedMonthOption.min).label;
+
+    const dateLabel = monthDaysOptions.find((d) => {
+      return d.value == selectedMonthOption.dom;
+    }).label;
 
     return (
       (this.state.selectedPeriod === 'month') &&
       <cron-month-component>
-        <select value={selectedMonthOption.day} onChange={this.onMonthOptionSelect('day')} disabled={disabled}>
-          {monthDaysOptions.map(this.getOptionComponent('month_days_option'))}
-        </select>
-        <span className='m-l-xs m-r-xs'>at</span>
-        <select value={selectedMonthOption.hour} onChange={this.onMonthOptionSelect('hour')} disabled={disabled}>
-          {hourOptions.map(this.getOptionComponent('hour_option'))}
-        </select>
-        :
-        <select value={selectedMonthOption.min} onChange={this.onMonthOptionSelect('min')} disabled={disabled}>
-          {minuteOptions.map(this.getOptionComponent('minute_option'))}
-        </select>
+        <div className="cron-field-row">
+          <span className='m-l-xs'>Date : </span>
+          {/*<select value={selectedMonthOption.dom} onChange={this.onMonthOptionSelect('dom')} disabled={disabled}>
+            {monthDaysOptions.map(this.getOptionComponent('month_days_option'))}
+          </select>*/}
+          <div className="cron-badge-option-container month-opt-container">
+            {this.getBadgeOptions(monthDaysOptions, selectedMonthOption.dom, 0, this.onMonthOptionSelect('dom'))}
+          </div>
+        </div>
+        <div className="cron-field-row">
+          <span className='m-l-xs'>Time : </span>
+          {this.getHourSelect(selectedMonthOption, this.onMonthOptionSelect('hour'))}
+          :
+          {this.getMinuteSelect(selectedMonthOption, this.onMonthOptionSelect('min'))}
+        </div>
+        {this.getTextComp(`${cronPeriodString} on ${dateLabel} at ${hourLabel}:${minuteLabel}`)}
       </cron-month-component>
     );
   }
 
-  getYearComponent = () => {
+  getYearComponent = (cronPeriodString) => {
     const {disabled} = this.props;
     const { monthOptions, monthDaysOptions, hourOptions, minuteOptions, selectedYearOption } = this.state;
+
+    const hourLabel = this.findHourOption(selectedYearOption.hour).label;
+    const minuteLabel = this.findMinuteOption(selectedYearOption.min).label;
+
+    const dateLabel = monthDaysOptions.find((d) => {
+      return d.value == selectedYearOption.dom;
+    }).label;
+    const monthLabel = monthOptions.find((d) => {
+      return d.value == selectedYearOption.mon;
+    }).label;
 
     return (
       (this.state.selectedPeriod === 'year') &&
       <cron-year-component>
-        <select value={selectedYearOption.day} onChange={this.onYearOptionSelect('day')} disabled={disabled}>
-          {monthDaysOptions.map(this.getOptionComponent('month_days_option'))}
-        </select>
-        <span className='m-l-xs m-r-xs'>of</span>
-        <select value={selectedYearOption.mon} onChange={this.onYearOptionSelect('mon')} disabled={disabled}>
-          {monthOptions.map(this.getOptionComponent('month_option'))}
-        </select>
-        <span className='m-l-xs m-r-xs'>at</span>
-        <select value={selectedYearOption.hour} onChange={this.onYearOptionSelect('hour')} disabled={disabled}>
-          {hourOptions.map(this.getOptionComponent('hour_option'))}
-        </select>
-        :
-        <select value={selectedYearOption.min} onChange={this.onYearOptionSelect('min')} disabled={disabled}>
-          {minuteOptions.map(this.getOptionComponent('minute_option'))}
-        </select>
+        <div className="cron-field-row">
+          <span className='m-l-xs'>Month : </span>
+          {/*<select value={selectedYearOption.mon} onChange={this.onYearOptionSelect('mon')} disabled={disabled}>
+            {monthOptions.map(this.getOptionComponent('month_option'))}
+          </select>*/}
+          <div className="cron-badge-option-container month-opt-container">
+            {this.getBadgeOptions(monthOptions, selectedYearOption.mon, 3, this.onYearOptionSelect('mon'))}
+          </div>
+        </div>
+        <div className="cron-field-row">
+          <span className='m-l-xs'>Date : </span>
+          {/*<select value={selectedYearOption.dom} onChange={this.onYearOptionSelect('dom')} disabled={disabled}>
+            {monthDaysOptions.map(this.getOptionComponent('month_days_option'))}
+          </select>*/}
+          <div className="cron-badge-option-container month-opt-container">
+            {this.getBadgeOptions(monthDaysOptions, selectedYearOption.dom, 0, this.onYearOptionSelect('dom'))}
+          </div>
+        </div>
+        <div className="cron-field-row">
+          <span className='m-l-xs'>Time : </span>
+          {this.getHourSelect(selectedYearOption, this.onYearOptionSelect('hour'))}
+          :
+          {this.getMinuteSelect(selectedYearOption, this.onYearOptionSelect('min'))}
+        </div>
+        {this.getTextComp(`${cronPeriodString} on ${dateLabel} of ${monthLabel} at ${hourLabel}:${minuteLabel}`)}
       </cron-year-component>
     );
   }
@@ -434,17 +548,17 @@ export default class ReactCron extends Component {
         min: 0
       },
       selectedWeekOption: {
-        day: 1,
+        dow: 1,
         hour: 0,
         min: 0
       },
       selectedMonthOption: {
-        day: 1,
+        dom: 1,
         hour: 0,
         min: 0
       },
       selectedYearOption: {
-        day: 1,
+        dom: 1,
         mon: 1,
         hour: 0,
         min: 0
@@ -457,7 +571,7 @@ export default class ReactCron extends Component {
       "min"  : d[0],
       "hour"  : d[1],
       "dom"   : d[2],
-      "month" : d[3],
+      "mon" : d[3],
       "dow"   : d[4]
     };
 
@@ -503,31 +617,37 @@ export default class ReactCron extends Component {
     const { className, disabled } = this.props;
     const { selectedPeriod, periodOptions } = this.state;
 
+    const option = periodOptions.find((o) => (o.value === selectedPeriod));
     const getPeriodPrep = () => {
-      const option = periodOptions.find((o) => (o.value === selectedPeriod));
       return (
-        <span className='m-l-xs m-r-xs'>{option.prep}</span>
+        <span className=''>{option.prep}</span>
       );
     };
+
+    const startText = 'Repeat every';
+    const cronPeriodString = `${startText} ${selectedPeriod}`;
 
     return (
       <div className={`${className} cron-row`}>
         <div className=''>
           <div className=''>
-            Every
-            <select value={selectedPeriod} onChange={this.onPeriodSelect()} disabled={disabled} className='m-l-xs'>
-              {periodOptions.map((t,index) => {
-                return (
-                  <option key={`period_option_${index}`} value={t.value}>{t.label}</option>
-                );
-              })}
-            </select>
-            {getPeriodPrep()}
-            {this.getHourComponent()}
-            {this.getDayComponent()}
-            {this.getWeekComponent()}
-            {this.getMonthComponent()}
-            {this.getYearComponent()}
+            <div className="cron-field-row">
+              <span className='m-l-xs'>Period :</span>
+              <select value={selectedPeriod} onChange={this.onPeriodSelect()} disabled={disabled} className='m-l-xs'>
+                {periodOptions.map((t,index) => {
+                  return (
+                    <option key={`period_option_${index}`} value={t.value}>{t.label}</option>
+                  );
+                })}
+              </select>
+            </div>
+            {/* getPeriodPrep() */}
+            {this.getMinuteComponent(cronPeriodString)}
+            {this.getHourComponent(cronPeriodString)}
+            {this.getDayComponent(cronPeriodString)}
+            {this.getWeekComponent(cronPeriodString)}
+            {this.getMonthComponent(cronPeriodString)}
+            {this.getYearComponent(cronPeriodString)}
           </div>
           {/*<input type='text' readOnly name='cron_tab' className='cron-input' value={getCron(this.state)}/>*/}
         </div>

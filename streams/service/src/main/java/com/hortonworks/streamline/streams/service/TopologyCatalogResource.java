bc7340c4104f8f29800d04a6d7299f6375fe30a5
Sriharsha Chintalapani <sriharsha@apache.org>
2018-11-28 19:25:35 -0800

    Fix empty dagThumbnail.

--- streams/service/src/main/java/com/hortonworks/streamline/streams/service/TopologyCatalogResource.java
+++ streams/service/src/main/java/com/hortonworks/streamline/streams/service/TopologyCatalogResource.java
@@ -526,6 +526,9 @@ public class TopologyCatalogResource {
             if (versionInfo.getDescription() == null) {
                 versionInfo.setDescription("");
             }
+            if (versionInfo.getDagThumbnail() == null) {
+                versionInfo.setDagThumbnail("");
+            }
             TopologyVersion savedVersion = catalogService.addOrUpdateTopologyVersionInfo(
                     currentVersion.get().getId(), versionInfo);
             catalogService.cloneTopologyVersion(topologyId, savedVersion.getId());

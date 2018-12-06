57d3381eac7d316b82c636c5bb6394c9adef42b4
Sriharsha Chintalapani <sriharsha@apache.org>
2018-12-05 16:18:31 -0800

    Increase JVM mem to 4G.
    Reviewers: bzzhang
    
    Reviewed By: bzzhang
    
    Differential Revision: https://code.uberinternal.com/D2331453

--- conf/streamline-env.sh
+++ conf/streamline-env.sh
@@ -3,3 +3,4 @@
 export STREAMLINE_USER=`whoami`
 export HTTP_HEADERS_FOR_CURL='-HX-Auth-Params-Email:root@uber.com'
 export HTTP_HEADERS_FOR_OTHERS='-H X-Auth-Params-Email:root@uber.com'
+export STREAMLINE_HEAP_OPTS='-Xmx4G -Xms4G'

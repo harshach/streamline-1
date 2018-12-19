fcbfcc45100034f03d832ce800e14df15978af84
Patrick Cullen <pcullen@uber.com>
2018-12-18 17:28:09 -0800

    Temporary connection hint workaround for demo
    Summary:
    The connection hint code doesn't work with task_params prefix, eg. task_params.hive_cli_conn_id.
    This changes the UI hint code to ignore this special case.
    
    Differential Revision: https://code.uberinternal.com/D2373705

--- streams/cluster/src/main/java/com/hortonworks/streamline/streams/cluster/bundle/impl/PiperHiveTaskBundleHintProvider.java
+++ streams/cluster/src/main/java/com/hortonworks/streamline/streams/cluster/bundle/impl/PiperHiveTaskBundleHintProvider.java
@@ -3,5 +3,5 @@ package com.hortonworks.streamline.streams.cluster.bundle.impl;
 public class PiperHiveTaskBundleHintProvider extends PiperTaskBundleHintProvider {
     public String getConnectionType() { return "hive_cli"; }
 
-    public String getConnectionFieldName() { return "hive_cli_conn_id"; }
+    public String getConnectionFieldName() { return "task_params.hive_cli_conn_id"; }
 }

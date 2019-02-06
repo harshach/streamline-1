49ed04465a7306c9e050b749743567c5b8e94535
Ankit Mody <amody@uber.com>
2019-02-06 00:30:58 -0800

    Refactor the spark and py-spark tasks
    Summary:
    Made spark_connection_id optional (will rely on owner or proxy_user instead)
    Added various spark options as required parameters instead of having the user enter it in keyvalue form.
    
    Resolves T2391003
    
    Reviewers: #uworc, pcullen
    
    Reviewed By: #uworc, pcullen
    
    Subscribers: harshach, jenkins
    
    Maniphest Tasks: T2391003
    
    Differential Revision: https://code.uberinternal.com/D2483513

--- streams/runners/piper/layout/src/main/java/com/hortonworks/streamline/streams/layout/piper/SparkTaskComponent.java
+++ streams/runners/piper/layout/src/main/java/com/hortonworks/streamline/streams/layout/piper/SparkTaskComponent.java
@@ -3,29 +3,31 @@ package com.hortonworks.streamline.streams.layout.piper;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hortonworks.streamline.streams.piper.common.pipeline.Task;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
  * Spark Task Component (used both for PySpark and Spark).
  */
 public class SparkTaskComponent extends GenericPiperTaskComponent {
-
     private static final Logger LOG = LoggerFactory.getLogger(SparkTaskComponent.class);
 
-    protected static final String TASK_SPARK_ARGS = TASK_ARGS_PREFIX + '.' + "spark_args";
-    protected static final String TASK_SPARK_OPTS = TASK_ARGS_PREFIX + '.' + "spark_opts";
-    protected static final String TASK_SPARK_ENV_ADDITIONS = TASK_ARGS_PREFIX + '.' + "env_additions";
-    protected static final String TASK_SPARK_USE_DROGON = "use_drogon";
-    protected static final String SPARK_COMPUTE_SERVICE_ENV = "spark_compute_service";
-    protected static final String SPARK_OPTS_KEY_PREFIX = "--";
+    private static final String TASK_SPARK_ARGS = TASK_ARGS_PREFIX + '.' + "spark_args";
+    private static final String TASK_SPARK_OPTS = TASK_ARGS_PREFIX + '.' + "spark_opts";
+    private static final String TASK_SPARK_ENV_ADDITIONS = TASK_ARGS_PREFIX + '.' + "env_additions";
+    private static final String TASK_SPARK_VERSION = TASK_ARGS_PREFIX + "." + "spark_version";
+    private static final String SPARK_VERSION_ENV = "SPARK_VERSION";
+    private static final String SPARK_OPTS_KEY_PREFIX = "--";
+    private static final List<String> PREDEFINED_SPARK_OPTS = Arrays
+        .asList("queue", "num_executors", "executor_cores", "executor_memory", "driver_cores",
+            "driver_memory");
 
     private static ObjectMapper objectMapper = new ObjectMapper();
 
@@ -33,10 +35,11 @@ public class SparkTaskComponent extends GenericPiperTaskComponent {
     public Task generateTask() {
 
         Task task = super.generateTask();
+        setPredefinedSparkOpts(task);
         setSparkOpts(task);
         setSparkArgs(task);
         setSparkEnvAdditions(task);
-        setDrogonOption(task);
+        setSparkVersion(task);
         return task;
     }
 
@@ -59,7 +62,7 @@ public class SparkTaskComponent extends GenericPiperTaskComponent {
 
     private void setSparkOpts(Task task) {
         if (config.containsKey(TASK_SPARK_OPTS)) {
-            Map sparkOpts = new HashMap<String, String>();
+            Map sparkOpts = (Map)task.getTaskParams().getParams().get(stripPrefix(TASK_SPARK_OPTS));
             Map kv = parseKeyValue((String)config.get(TASK_SPARK_OPTS));
             for (Object e: kv.entrySet()) {
                 Map.Entry<String, String> entry = (Map.Entry<String, String>) e;
@@ -71,8 +74,17 @@ public class SparkTaskComponent extends GenericPiperTaskComponent {
                     sparkOpts.put(key, entry.getValue());
                 }
             }
-            task.getTaskParams().setParams(stripPrefix(TASK_SPARK_OPTS), sparkOpts);
         }
+    }
+
+    private void setPredefinedSparkOpts(Task task) {
+        Map sparkOpts = new HashMap<String, Object>();
+        for (String predefinedProperty : PREDEFINED_SPARK_OPTS) {
+            Object value = config.get(TASK_SPARK_OPTS + "." + predefinedProperty);
+            String predefinedPropertyWithUnderscore = predefinedProperty.replaceAll("_", "-");
+            sparkOpts.put(SPARK_OPTS_KEY_PREFIX + predefinedPropertyWithUnderscore, value);
+        }
+        task.getTaskParams().setParams(stripPrefix(TASK_SPARK_OPTS), sparkOpts);
     }
 
     private void setSparkEnvAdditions(Task task) {
@@ -84,12 +96,11 @@ public class SparkTaskComponent extends GenericPiperTaskComponent {
         }
     }
 
-    private void setDrogonOption(Task task) {
-        if (config.containsKey(TASK_SPARK_USE_DROGON)) {
-            if ((Boolean)config.get(TASK_SPARK_USE_DROGON)) {
-                Map envAdditions = (HashMap<String, String>)task.getTaskParams().getParams().get(stripPrefix(TASK_SPARK_ENV_ADDITIONS));
-                envAdditions.put(SPARK_COMPUTE_SERVICE_ENV, true);
-            }
+    private void setSparkVersion(Task task) {
+        if (config.containsKey(TASK_SPARK_VERSION)) {
+            Map envAdditions = (HashMap<String, String>) task.getTaskParams().getParams()
+                .get(stripPrefix(TASK_SPARK_ENV_ADDITIONS));
+            envAdditions.put(SPARK_VERSION_ENV, config.get(TASK_SPARK_VERSION));
         }
     }
 

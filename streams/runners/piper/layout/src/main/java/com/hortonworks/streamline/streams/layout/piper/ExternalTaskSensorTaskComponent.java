8f974660f46d547ea2902e6bc9ec473d7522fe7e
Ankit Mody <amody@uber.com>
2019-02-12 14:07:26 -0800

    Refactor the external task sensor
    Summary:
    Removed the Delta days, hours, poke interval, execution timeout.
    Renamed execution delta to Look back minutes.
    
    Resolves T2393841
    
    Reviewers: #uworc, harshach
    
    Reviewed By: #uworc, harshach
    
    Maniphest Tasks: T2393841
    
    Differential Revision: https://code.uberinternal.com/D2504505

--- streams/runners/piper/layout/src/main/java/com/hortonworks/streamline/streams/layout/piper/ExternalTaskSensorTaskComponent.java
+++ streams/runners/piper/layout/src/main/java/com/hortonworks/streamline/streams/layout/piper/ExternalTaskSensorTaskComponent.java
@@ -2,21 +2,13 @@ package com.hortonworks.streamline.streams.layout.piper;
 
 import com.hortonworks.streamline.streams.piper.common.pipeline.Task;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * ExternalTaskSensor component.
  */
 public class ExternalTaskSensorTaskComponent extends GenericPiperTaskComponent {
-    private static final String DELTA_DIRECTION = "execution_delta.direction";
     private static final String DELTA_MINUTES = "execution_delta.minutes";
-    private static final String DELTA_HOURS = "execution_delta.hours";
-    private static final String DELTA_DAYS = "execution_delta.days";
     private static final String ALLOWED_STATES = "allowed_states";
-    private static final String MATCH_BEFORE_DELTA = "before";
     private static final String MATCH_SUCCESS_STATE = "success";
-
     private static final String SENSOR_OPT_EXECUTION_DELTA = "execution_delta";
     private static final String SENSOR_OPT_ALLOWED_STATES = "allowed_states";
     private static final String SENSOR_OPT_FAILED_STATES[] = new String[] {"failed", "shutdown", "upstream_failed", "skipped"};
@@ -47,19 +39,8 @@ public class ExternalTaskSensorTaskComponent extends GenericPiperTaskComponent {
         if (config.containsKey(DELTA_MINUTES)) {
             delta += (Integer)config.get(DELTA_MINUTES);
         }
-        if (config.containsKey(DELTA_HOURS)) {
-            delta += (Integer)config.get(DELTA_HOURS) * 60;
-        }
-        if (config.containsKey(DELTA_DAYS)) {
-            delta += (Integer)config.get(DELTA_DAYS) * 60 * 24;
-        }
-        boolean isBeforeDelta = ((String)config.get(DELTA_DIRECTION)).toLowerCase().contains(MATCH_BEFORE_DELTA);
 
-        if (isBeforeDelta) {
-            delta = Math.abs(delta) * -1;
-        } else {
-            delta = Math.abs(delta);
-        }
+        delta = Math.abs(delta) * -1;
         task.getTaskParams().setParams(SENSOR_OPT_EXECUTION_DELTA, delta);
     }
 }

9e69e9c6127573e5c074677fdb3cb60545a6a85c
Sriharsha Chintalapani <sriharsha@apache.org>
2019-02-13 08:41:06 -0800

    T2526243: Add code editor for shell
    Reviewers: pcullen
    
    Reviewed By: pcullen
    
    Maniphest Tasks: T2526243
    
    Differential Revision: https://code.uberinternal.com/D2507921

--- common/src/main/java/com/hortonworks/streamline/common/ComponentUISpecification.java
+++ common/src/main/java/com/hortonworks/streamline/common/ComponentUISpecification.java
@@ -65,7 +65,7 @@ public class ComponentUISpecification {
         STRING("string"), ENUMSTRING("enumstring"), ARRAYSTRING("array.string"), ARRAYENUMSTRING("array.enumstring"),
         NUMBER("number"), ARRAYNUMBER("array.number"), BOOLEAN("boolean"), ARRAYBOOLEAN("array.boolean"),
         OBJECT("object"), ENUMOBJECT("enumobject"), ARRAYOBJECT("array.object"), ARRAYENUMOBJECT("array.enumobject"),
-        FILE("file"), DATE("date"), DATETIME("datetime"), SQL("sql"), KEYVALUE("keyvalue"), CRONTAB("cron"), USER("user");
+        FILE("file"), DATE("date"), DATETIME("datetime"), SQL("sql"), KEYVALUE("keyvalue"), CRONTAB("cron"), SHELL("shell");
 
         private String uiFieldTypeText;
 
@@ -341,7 +341,8 @@ public class ComponentUISpecification {
 
         private void validateStringField () throws ComponentConfigException {
             UIFieldType[] stringTypes = {UIFieldType.STRING, UIFieldType.ENUMSTRING, UIFieldType.ARRAYSTRING, UIFieldType.ARRAYENUMSTRING,
-                                         UIFieldType.DATE, UIFieldType.DATETIME, UIFieldType.SQL, UIFieldType.KEYVALUE, UIFieldType.CRONTAB, UIFieldType.USER};
+                                         UIFieldType.DATE, UIFieldType.DATETIME, UIFieldType.SQL, UIFieldType.KEYVALUE,
+                                         UIFieldType.CRONTAB, UIFieldType.SHELL};
             if (!Arrays.asList(stringTypes).contains(this.type)) {
                 return;
             }

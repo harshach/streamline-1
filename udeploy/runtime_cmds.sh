292ebb2fc7a433731a5ac122376f40a5f7557427
Bo Zhang <bzzhang@uber.com>
2019-04-08 11:46:02 -0700

    Update version to 0.1.0-beta

--- udeploy/runtime_cmds.sh
+++ udeploy/runtime_cmds.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-/home/udocker/uworc/hortonworks-streamline-0.6.0-SNAPSHOT/bootstrap/bootstrap-storage.sh migrate
-/home/udocker/uworc/hortonworks-streamline-0.6.0-SNAPSHOT/bin/streamline start
+/home/udocker/uworc/hortonworks-streamline-0.1.0-beta/bootstrap/bootstrap-storage.sh migrate
+/home/udocker/uworc/hortonworks-streamline-0.1.0-beta/bin/streamline start
 
 echo 'waiting for UI...'
 until $(curl --output /dev/null --silent --head --fail http://localhost:${UBER_PORT_HTTP}); do
@@ -12,8 +12,8 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:${UBER_P
 done
 echo ""
 
-/home/udocker/uworc/hortonworks-streamline-0.6.0-SNAPSHOT/bootstrap/bootstrap.sh migrate
-/home/udocker/uworc/hortonworks-streamline-0.6.0-SNAPSHOT/bin/streamline stop
+/home/udocker/uworc/hortonworks-streamline-0.1.0-beta/bootstrap/bootstrap.sh migrate
+/home/udocker/uworc/hortonworks-streamline-0.1.0-beta/bin/streamline stop
 
 # Starting the streamline server in foreground
-/home/udocker/uworc/hortonworks-streamline-0.6.0-SNAPSHOT/bin/streamline start-fg
+/home/udocker/uworc/hortonworks-streamline-0.1.0-beta/bin/streamline start-fg

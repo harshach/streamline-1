1772bf68d5b78f0507a50d217ab9dfddb7b27b70
Bo Zhang <bzzhang@uber.com>
2019-05-01 09:14:42 -0700

    Fix convertAvroSchemaToStreamlineSchema for schemas with array/map/record fields
    Reviewers: #uworc, harshach
    
    Reviewed By: #uworc, harshach
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D2783129

--- streams/service/src/main/java/com/hortonworks/streamline/streams/service/AvroStreamlineSchemaConverter.java
+++ streams/service/src/main/java/com/hortonworks/streamline/streams/service/AvroStreamlineSchemaConverter.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
 import com.hortonworks.registries.common.Schema;
-import com.hortonworks.streamline.streams.StreamlineEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,7 +51,7 @@ public class AvroStreamlineSchemaConverter {
         org.apache.avro.Schema avroSchema = new org.apache.avro.Schema.Parser().parse(avroSchemaText);
         LOG.debug("Generating streams schema for given avro schema [{}]", avroSchemaText);
 
-        Schema.Field field = generateStreamsSchemaField(avroSchema);
+        Schema.Field field = generateStreamsSchemaField(PRIMITIVE_PAYLOAD_FIELD, avroSchema);
         List<Schema.Field> effFields;
         // root record is expanded directly as streamline schema represents that as the root element schema
         if (field instanceof Schema.NestedField) {
@@ -163,21 +162,29 @@ public class AvroStreamlineSchemaConverter {
         return avroSchema;
     }
 
-    private static Schema.Field generateStreamsSchemaField(org.apache.avro.Schema avroSchema) {
+    private static Schema.Field generateStreamsSchemaField(String name, org.apache.avro.Schema avroSchema) {
         Schema.Field effField = null;
         Schema.Type fieldType = null;
         boolean isPrimitive = true;
         boolean isOptional = false;
-        org.apache.avro.Schema.Type avroSchemaType = avroSchema.getType();
-        LOG.debug("Schema type: [{}]", avroSchemaType);
-        switch (avroSchemaType) {
+        if (avroSchema.getType() == org.apache.avro.Schema.Type.UNION) {
+            List<org.apache.avro.Schema> schemaTypes = avroSchema.getTypes();
+            Preconditions.checkArgument(schemaTypes != null && schemaTypes.size() == 2);
+            Preconditions.checkArgument(schemaTypes.get(0).getType().equals(org.apache.avro.Schema.Type.NULL));
+            avroSchema = avroSchema.getTypes().get(1);
+            isOptional = true;
+        }
+        LOG.debug("Schema type: [{}]", avroSchema.getType());
+        switch (avroSchema.getType()) {
             case MAP:
-                fieldType = Schema.Type.NESTED;
+                isPrimitive = false;
+                effField = generateMapSchema(avroSchema, name, isOptional);
+                LOG.debug("Generated map field [{}]", effField);
                 break;
             case ARRAY:
                 isPrimitive = false;
                 LOG.debug("Encountered array field and creating respective member fields");
-                effField = generateArraySchema(null, avroSchema);
+                effField = generateArraySchema(avroSchema, name, isOptional);
                 LOG.debug("Generated array field [{}]", effField);
                 break;
             case ENUM:
@@ -209,118 +216,38 @@ public class AvroStreamlineSchemaConverter {
             case RECORD:
                 isPrimitive = false;
                 LOG.debug("Encountered record field and creating respective nested field");
-                effField = generateRecordSchema(avroSchema);
-                LOG.debug("Generated nested field [{}] for avro fields [{}]", effField);
-                break;
-            case UNION:
-                // supporting unions with null and a type only, viz is optional values for a type.
-                fieldType = getEffectiveUnionSchemas(avroSchema);
-                isOptional = true;
+                effField = generateRecordSchema(avroSchema, name, isOptional);
+                LOG.debug("Generated nested field [{}]", effField);
                 break;
             default:
                 throw new IllegalArgumentException("Given type " + avroSchema + " is not supported");
         }
 
         if (isPrimitive) {
-            effField = isOptional ? Schema.Field.optional(PRIMITIVE_PAYLOAD_FIELD, fieldType)
-                    : Schema.Field.of(PRIMITIVE_PAYLOAD_FIELD, fieldType);
+            effField = isOptional ? Schema.Field.optional(name, fieldType)
+                    : Schema.Field.of(name, fieldType);
         }
 
         return effField;
     }
 
-    private static Schema.Type getEffectiveUnionSchemas(org.apache.avro.Schema avroSchema) {
-        Schema.Type fieldType;
-        List<org.apache.avro.Schema> avroSchemaTypes = avroSchema.getTypes();
-        if (avroSchemaTypes.size() > 2) {
-            throw new IllegalArgumentException("Unions with more than two types is not supported.");
-        }
-        org.apache.avro.Schema type1 = avroSchemaTypes.get(0);
-        org.apache.avro.Schema type2 = avroSchemaTypes.get(1);
-        if (org.apache.avro.Schema.Type.NULL.equals(type1.getType())) {
-            fieldType = getStreamsSchemaFieldType(type2);
-        } else if (org.apache.avro.Schema.Type.NULL.equals(type2.getType())) {
-            fieldType = getStreamsSchemaFieldType(type1);
-        } else {
-            throw new IllegalArgumentException("One of the two types in union must be `null`.");
-        }
-        return fieldType;
-    }
-
-    private static Schema.ArrayField generateArraySchema(String name, org.apache.avro.Schema avroSchema) {
-        return Schema.ArrayField.of(name, generateStreamsSchemaField(avroSchema.getElementType()));
-    }
-
-    private static Schema.NestedField generateRecordSchema(org.apache.avro.Schema avroSchema) {
-        return generateRecordSchema(avroSchema, avroSchema.getName());
-    }
-
-    private static Schema.NestedField generateRecordSchema(org.apache.avro.Schema avroSchema, String name) {
+    private static Schema.NestedField generateRecordSchema(org.apache.avro.Schema avroSchema, String name, boolean isOptional) {
         List<org.apache.avro.Schema.Field> avroFields = avroSchema.getFields();
         List<Schema.Field> fields = new ArrayList<>();
         for (org.apache.avro.Schema.Field avroField : avroFields) {
-            if (avroField.schema().getType() == org.apache.avro.Schema.Type.RECORD) {
-                LOG.debug("Encountered record field and creating respective nested fields");
-                fields.add(generateRecordSchema(avroField.schema(), avroField.name()));
-            } else if (avroField.schema().getType() == org.apache.avro.Schema.Type.ARRAY) {
-                LOG.debug("Encountered array field and creating respective member fields");
-                fields.add(generateArraySchema(avroField.name(), avroField.schema()));
-            } else {
-                boolean isOptional = org.apache.avro.Schema.Type.UNION.equals(avroField.schema().getType());
-                Schema.Field field = isOptional ? Schema.Field.optional(avroField.name(), getStreamsSchemaFieldType(avroField.schema()))
-                                                : Schema.Field.of(avroField.name(), getStreamsSchemaFieldType(avroField.schema()));
-                fields.add(field);
-            }
+            fields.add(generateStreamsSchemaField(avroField.name(), avroField.schema()));
         }
-
-        return Schema.NestedField.optional(name, fields);
+        return isOptional ? Schema.NestedField.optional(name, fields)
+                : Schema.NestedField.of(name, fields);
     }
 
-    private static Schema.Type getStreamsSchemaFieldType(org.apache.avro.Schema avroSchema) {
-        org.apache.avro.Schema.Type avroSchemaType = avroSchema.getType();
-        Schema.Type fieldType;
-        switch (avroSchemaType) {
-            case MAP:
-                fieldType = Schema.Type.NESTED;
-                break;
-            case ARRAY:
-                fieldType = Schema.Type.ARRAY;
-                break;
-            case ENUM:
-            case STRING:
-                fieldType = Schema.Type.STRING;
-                break;
-            case BOOLEAN:
-                fieldType = Schema.Type.BOOLEAN;
-                break;
-            case INT:
-                fieldType = Schema.Type.INTEGER;
-                break;
-            case FLOAT:
-                fieldType = Schema.Type.FLOAT;
-                break;
-            case LONG:
-                fieldType = Schema.Type.LONG;
-                break;
-            case DOUBLE:
-                fieldType = Schema.Type.DOUBLE;
-                break;
-            case BYTES:
-            case FIXED:
-                fieldType = Schema.Type.BINARY;
-                break;
-            case RECORD:
-            case NULL:
-                fieldType = Schema.Type.NESTED;
-                break;
-            case UNION:
-                fieldType = getEffectiveUnionSchemas(avroSchema);
-                break;
-            default:
-                throw new IllegalArgumentException("Given type " + avroSchema + " is not supported");
-        }
-
-        return fieldType;
+    private static Schema.ArrayField generateArraySchema(org.apache.avro.Schema avroSchema, String name, boolean isOptional) {
+        return isOptional ? Schema.ArrayField.optional(name, generateStreamsSchemaField(null, avroSchema.getElementType()))
+                : Schema.ArrayField.of(name, generateStreamsSchemaField(null, avroSchema.getElementType()));
     }
 
+    private static Schema.NestedField generateMapSchema(org.apache.avro.Schema avroSchema, String name, boolean isOptional) {
+        return isOptional ? Schema.NestedField.optional(name, generateStreamsSchemaField(null, avroSchema.getValueType()))
+                : Schema.NestedField.of(name, generateStreamsSchemaField(null, avroSchema.getValueType()));
+    }
 }
